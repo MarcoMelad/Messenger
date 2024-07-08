0601e17e93f76dc9df1eb8a81ca00f043565089d
Marco Melad <marcomelad2022@gmail.com>
2024-07-08 20:48:09 +0300

    init files

---
+++ app/Models/Conversation.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Conversation extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id', 'label'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class, 'conversation_id', 'id')->latest();
+    }
+    public function participants(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'participants')
+            ->withPivot([
+                'role', 'joined_at'
+            ]);
+    }
+}
